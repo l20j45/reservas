31461f6ee4eadb4f421eb89c77a06c0a54c2129d
Daniel Rojas <l20j45_@gmail.com>
2025-06-01 15:08:24 -0600

    Add migration files to rename status column and create reservations_detail table

---
+++ database/migrations/2025_06_01_165135_create_reservations_detail_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reservations_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('reservation_id')
+                ->references('id')
+                ->on('reservation')
+                ->constrained('reservation')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->string('transaction_id')->nullable();
+            $table->string('payer_id')->nullable();
+            $table->string('payer_email')->nullable();
+            $table->string('payment_status')->nullable();
+            $table->decimal('amount', 10, 2)->nullable();
+            $table->string('response_json', 3);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations_detail');
+    }
+};
