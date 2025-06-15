56004995f3299316fe64d5aeed8bf0bbe49e1b6f
Daniel Rojas <l20j45_@gmail.com>
2025-06-15 13:51:24 -0600

    Refactor topbar and app layout: Removed unused dropdowns and customizer settings for a cleaner UI. Updated user display to show authenticated user's name and email.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -77,12 +77,7 @@
     </button>
     <!--end back-to-top-->
 
-    <div class="customizer-setting d-none d-md-block">
-        <div class="btn-info btn-rounded shadow-lg btn btn-icon btn-lg p-2" data-bs-toggle="offcanvas"
-            data-bs-target="#theme-settings-offcanvas" aria-controls="theme-settings-offcanvas">
-            <i class='mdi mdi-spin mdi-cog-outline fs-22'></i>
-        </div>
-    </div>
+
 </body>
 
 
